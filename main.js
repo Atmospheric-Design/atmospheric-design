a122b1e332e80c0c86616abad282e0b24ab26b51
Bozhan Chipev <bchipev@gmail.com>
2020-04-21 21:31:11 +0200

    fixed overlays and links on all screen sizes

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 const topLeftImage = document.querySelector(".top-left-image");
+const interiorLink = document.querySelector(".interior-link");
+
 const topRightImage = document.querySelector(".top-right-image");
+const furnitureLink = document.querySelector(".furniture-link");
+
 const bottomLeftImage = document.querySelector(".bottom-left-image");
+const exhibitionLink = document.querySelector(".exhibition-link");
+
 const bottomRightImage = document.querySelector(".bottom-right-image");
+const experimentLink = document.querySelector(".experiment-link");
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
+  console.log("js loaded");
 
-    function addHoverEffect(element, value) {
-        element.addEventListener("mouseover", function() {
-            let overlay = document.querySelectorAll(".overlay")[value];
-            overlay.style.opacity = "0.9";
-            let overlaySmall = document.querySelectorAll(".overlay-small")[value];
-            overlaySmall.style.opacity="1";
-        })
-    
-        element.addEventListener("mouseout", function() {
-            let overlay = document.querySelectorAll(".overlay")[value];
-            overlay.style.opacity = "0";
-            let overlaySmall = document.querySelectorAll(".overlay-small")[value];
-            overlaySmall.style.opacity="0";
+  function addHoverEffect(link) {
+    console.log("added hover effect on " + link);
 
-            
-        })};
+    link.addEventListener("mouseover", function () {
+      this.querySelector(".overlay").style.opacity = "0.9";
+      this.querySelector(".overlay-small").style.opacity = "0.3";
+    });
 
-    addHoverEffect(topLeftImage, 0);
-    addHoverEffect(topRightImage, 1);
-    addHoverEffect(bottomLeftImage, 2);
-    addHoverEffect(bottomRightImage, 3);
+    link.addEventListener("mouseout", function () {
+      this.querySelector(".overlay").style.opacity = "0";
+      this.querySelector(".overlay-small").style.opacity = "0";
+    });
+  }
 
-})
+  addHoverEffect(interiorLink);
+  addHoverEffect(furnitureLink);
+  addHoverEffect(exhibitionLink);
+  addHoverEffect(experimentLink);
+});
