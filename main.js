b3f153d759bb893eef436b80eac19ee854b8cc65
Bozhan Chipev <bchipev@gmail.com>
2020-05-09 18:34:37 +0200

    new layout

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-const topLeftImage = document.querySelector(".top-left-image");
-const interiorLink = document.querySelector(".interior-link");
-
-const topRightImage = document.querySelector(".top-right-image");
-const furnitureLink = document.querySelector(".furniture-link");
-
-const bottomLeftImage = document.querySelector(".bottom-left-image");
-const exhibitionLink = document.querySelector(".exhibition-link");
-
-const bottomRightImage = document.querySelector(".bottom-right-image");
-const experimentLink = document.querySelector(".experiment-link");
+const projects = document.querySelectorAll(".project");
+const closeButton = document.querySelector(".close-button");
+const shopNew = document.querySelector(".shop-new");
 
 window.addEventListener("load", function () {
-  console.log("js loaded");
-
-  function addHoverEffect(link) {
-    console.log("added hover effect on " + link);
-
-    link.addEventListener("mouseover", function () {
-      this.querySelector(".overlay").style.opacity = "0.9";
-      this.querySelector(".overlay-small").style.opacity = "0.3";
+  function addHoverEffect(project) {
+    project.addEventListener("mouseover", function () {
+      this.querySelector("img").style.outline = "solid black 2px";
+      this.querySelector("img").style.outlineOffset = "-2px";
+      this.style.outline = "solid black 2px";
+      this.style.outlineOffset = "-2px";
+      this.querySelector(".arrow").style.visibility = "visible";
     });
 
-    link.addEventListener("mouseout", function () {
-      this.querySelector(".overlay").style.opacity = "0";
-      this.querySelector(".overlay-small").style.opacity = "0";
+    project.addEventListener("mouseout", function () {
+      this.querySelector("img").style.outline = "none";
+      this.querySelector("img").style.outlineOffset = "0";
+      this.style.outline = "none";
+      this.style.outlineOffset = "0";
+      this.querySelector(".arrow").style.visibility = "hidden";
     });
   }
 
-  addHoverEffect(interiorLink);
-  addHoverEffect(furnitureLink);
-  addHoverEffect(exhibitionLink);
-  addHoverEffect(experimentLink);
+  addHoverEffect(projects[0]);
+  addHoverEffect(projects[1]);
+  addHoverEffect(projects[2]);
+  addHoverEffect(projects[3]);
+
+  function shopNewCloseButton() {
+    closeButton.addEventListener("click", function () {
+      shopNew.style.visibility = "hidden";
+    });
+  }
+  shopNewCloseButton();
 });
