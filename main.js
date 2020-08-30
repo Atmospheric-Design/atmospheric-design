a7863d97787307207cd3e1d2086ee046e748a672
Bozhan Chipev <bchipev@gmail.com>
2020-08-30 19:46:05 +0200

    fixing shop-new

--- main.js
+++ main.js
@@ -7,18 +7,20 @@ window.addEventListener("load", function () {
     } else {
       const shopNew = document.createElement("DIV");
       shopNew.classList.add("shop-new");
-      shopNew.innerHTML = `<a href="#" class="close-button">X</a>
+      shopNew.innerHTML = `<a target="_blank" rel="noopener noreferrer" href="#" class="close-button">X</a>
         <h2>New on the shop</h2>
-        <a class="image-link" href="#"><img alt="" /></a>
+        <a target="_blank" rel="noopener noreferrer" class="image-link" href="#"><img alt="" /></a>
         <h3>#</h3>
-        <a href="/" class="price"></a>
-        <a href="/" class="details">View product details</a>`;
+        <a href="/" target="_blank" rel="noopener noreferrer" class="price"></a>
+        <a href="/" target="_blank" rel="noopener noreferrer" class="details">View product details</a>`;
       let aside = document.querySelector("aside");
       aside.appendChild(shopNew);
-      let shopLink = "/";
-      let shopIMG = "/assets/images/instrument-table.jpg";
-      let shopTitle = "Instrument table";
-      let shopPrice = "€ 329";
+      let shopLink =
+        "https://www.etsy.com/listing/860344897/minimal-macrame-shelf";
+      let shopIMG =
+        "/assets/images/furniture/minimal-macrame-shelf/minimal-macrame-shelf-full-view.jpg";
+      let shopTitle = "Minimal macrame shelf";
+      let shopPrice = "€ 99";
       shopNew.querySelector("img").src = shopIMG;
       shopNew.querySelector(".image-link").href = shopLink;
       shopNew.querySelector("h3").innerText = shopTitle;
