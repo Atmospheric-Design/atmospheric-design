0f17e9361a8f15c4a7ab881d44f5d6b6b05abb5c
Bozhan Chipev <bchipev@gmail.com>
2021-05-13 21:10:42 +0200

    changes

--- home.js
+++ home.js
@@ -3,18 +3,18 @@ const projects = document.querySelectorAll(".project");
 window.addEventListener("load", function () {
   function addHoverEffect(project) {
     project.addEventListener("mouseover", function () {
-      this.querySelector("img").style.outline = "solid black 2px";
-      this.querySelector("img").style.outlineOffset = "-2px";
-      this.style.outline = "solid black 2px";
-      this.style.outlineOffset = "-2px";
+      // this.querySelector("img").style.outline = "solid black 2px";
+      // this.querySelector("img").style.outlineOffset = "-2px";
+      // this.style.outline = "solid black 2px";
+      // this.style.outlineOffset = "-2px";
       this.querySelector(".arrow").style.visibility = "visible";
     });
 
     project.addEventListener("mouseout", function () {
-      this.querySelector("img").style.outline = "none";
-      this.querySelector("img").style.outlineOffset = "0";
-      this.style.outline = "none";
-      this.style.outlineOffset = "0";
+      // this.querySelector("img").style.outline = "none";
+      // this.querySelector("img").style.outlineOffset = "0";
+      // this.style.outline = "none";
+      // this.style.outlineOffset = "0";
       this.querySelector(".arrow").style.visibility = "hidden";
     });
   }
